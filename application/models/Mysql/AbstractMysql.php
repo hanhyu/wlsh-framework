12fc2c12a2f56f0fc8a34b9857a1c43699fe2c75
hanhyu <hanhyu@qq.com>
2019-05-27 18:24:45 +0800

    优化连接池,取消put方法，使用defer方法替代
    Signed-off-by: hanhyu <hanhyu@qq.com>

--- application/models/Mysql/AbstractMysql.php
+++ application/models/Mysql/AbstractMysql.php
@@ -2,6 +2,10 @@
 declare(strict_types=1);
 
 namespace App\Models\Mysql;
+
+use Medoo\Medoo;
+use Yaf\Registry;
+
 /**
  * Created by PhpStorm.
  * User: hanhyu
@@ -11,21 +15,14 @@ namespace App\Models\Mysql;
 abstract class AbstractMysql
 {
     /**
-     * @var \Medoo\Medoo
+     * @var Medoo
      */
     protected $db;
 
-    /**
-     * @param $method
-     * @param $args
-     *
-     * @return mixed
-     * @throws \Exception
-     */
     public function __call($method, $args)
     {
         try {
-            $this->db = \Yaf\Registry::get('mysql_pool')->get();
+            $this->db = Registry::get('mysql_pool')->get();
         } catch (\PDOException $e) {
             co_log($e->getMessage(), "mysql数据连接异常", 'alert');
             throw new \Exception('mysql数据连接异常', 500);
@@ -33,61 +30,23 @@ abstract class AbstractMysql
 
         $data = call_user_func_array([$this, $method], $args);
 
-        if (!empty($this->db)) {
-            \Yaf\Registry::get('mysql_pool')->put($this->db);
-        }
-
         return $data;
     }
-
-    /*public function __call($method, $args)
-    {
-        $chan = new \SplQueue();
-        go(function () use ($chan) {
-            try{
-                $chan->push(\Yaf\Registry::get('mysql_pool')->get());
-            } catch (\PDOException $e){
-                co_log($e->getMessage(), "mysql数据连接异常");
-                $chan->push($e->getMessage());
-            }
-
-        });
-
-        while (1) {
-            if (count($chan) > 0) {
-                $data = $chan->pop();
-                if (is_string($data)){
-                    throw new \Exception($data);
-                }
-                $this->db = $data;
-                break;
-            }
-        }
-
-        $data = call_user_func_array([$this, $method], $args);
-
-        go(function () {
-            \Yaf\Registry::get('mysql_pool')->put($this->db);
-        });
-
-        return $data;
-    }*/
 
     /*
      * 当new一个数据对象时，construct方式时qps为8863,call方式时qps8242,连接池都是占用12个
      * 当同时new十个数据对象时，construct方式时2835（占用池子84个）,call方式时还是跟一个对象一样8170(占用池子12个)
-     *  public function __construct()
-        {
-            //从数据库连接池中获取连接对象
-            $this->db = \Yaf\Registry::get('mysql_pool')->get();
+    public function __construct()
+    {
+        //从数据库连接池中获取连接对象
+        try {
+            $this->db = Registry::get('mysql_pool')->get();
+        } catch (\PDOException $e) {
+            co_log($e->getMessage(), "mysql数据连接异常", 'alert');
+            throw new \Exception('mysql数据连接异常', 500);
         }
-
-        public function __destruct()
-        {
-            //把用完的连接对象放回池子中供下一个连接重复使用
-            \Yaf\Registry::get('mysql_pool')->put($this->db);
-        }
-
+    }
     */
+
 
 }
